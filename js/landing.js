a79b9d45b560311e3ac22a142c69d93f0e791fd7
Narendras <n.syahrasyad.n@gmail.com>
2018-02-24 17:55:36 -0800

    Added 2nd page to COMM291

--- js/landing.js
+++ js/landing.js
@@ -1,7 +1,1 @@
-const fs = require("fs");
-const markdown = require("markdown-js");
 
-const str = fs.readFileSync("notes/src/comm/291/01.md", "utf8");
-const result = markdown.makeHtml(str);
-
-console.log(str);
