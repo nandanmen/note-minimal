bbfc351dcc794ff5f7e3888c360d0cc040cfd8e9
Narendras <n.syahrasyad.n@gmail.com>
2018-02-25 20:10:59 -0800

    Testing for smooth page transitions

--- js/note.js
+++ js/note.js
@@ -1,10 +1,39 @@
-const fs = require("fs");
-const md = require("markdown-js");
-
-(function main() {
-  const str = fs.readFileSync("notes/src/comm/291/01.md", "utf8");
-  const result = md.makeHtml(str);
-
-  const container = document.querySelector(".note");
-  container.innerHTML = result;
+(function addListeners() {
+  document.addEventListener("click", e => {
+    let el = e.target;
+    while (el && !el.href) {
+      el = el.parentNode;
+    }
+    if (el) {
+      e.preventDefault();
+      history.pushState(null, null, el.href);
+      changePage();
+      return;
+    }
+  });
+  window.addEventListener("popstate", changePage);
 }());
+
+function loadPage(url) {
+  return fetch(url, {
+    method: 'GET'
+  }).then(function(response) {
+    return response.text();
+  });
+}
+
+const main = document.querySelector("main");
+
+function changePage() {
+  const url = window.location.href;
+  loadPage(url).then(function(responseText) {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = responseText;
+
+    const oldContent = document.querySelector(".note");
+    const newContent = wrapper.querySelector(".note");
+
+    main.appendChild(newContent);
+    oldContent.parentNode.removeChild(oldContent);
+  })
+}
