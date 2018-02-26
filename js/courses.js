675c927ab8e25daca1a339f8ffa0a5e77d6584f4
Narendras <n.syahrasyad.n@gmail.com>
2018-02-25 22:43:25 -0800

    Added animations to landing

---
+++ js/courses.js
@@ -0,0 +1,35 @@
+const courses = [
+  {
+    dept: "cpsc",
+    classes: [
+      {
+        number: 210,
+        desc: "Software Construction",
+        lecs: []
+      },
+      {
+        number: 213,
+        desc: "Introduction to Computer Systems",
+        lecs: []
+      },
+      {
+        number: 221,
+        desc: "Beginner Algorithms and Data Structures",
+        lecs: []
+      }
+    ]
+  },
+  {
+    dept: "comm",
+    classes: [
+      {
+        number: 291,
+        desc: "Application of Statistics in Business",
+        lecs: [
+          "Numerical Summaries of Center and Spread",
+          "Correlation and Regression"
+        ]
+      }
+    ]
+  }
+];
--- js/landing.js
+++ js/landing.js
@@ -1,1 +1,29 @@
+(function build() {
+  let classCount = 0;
+  const linkTemplate = document.getElementById("course-link-template");
+  const container = document.querySelector(".course__list");
+
+  for (let i = 0; i < courses.length; i++) {
+    const currDept = courses[i];
+    const classes = currDept.classes;
+    for (let k = 0; k < classes.length; k++) {
+      const currClass = classes[k];
+      const number = currClass.number;
+      classCount++;
+
+      const a = linkTemplate.content.querySelector(".course a");
+      a.textContent = number;
+      a.href = `notes/${currDept.dept}/${number}`;
+      setAnimation(a, classCount);
+
+      const result = document.importNode(linkTemplate.content, true);
+      container.appendChild(result);
+    }
+  }
+}());
+
+function setAnimation(node, delay) {
+  node.style["animation-delay"] = `${1.5 + delay/10}s`;
+}
+
 
